cdaa7052731e5426377b78d770f235490864d8f0
Vedant Rishi Das <55937481+vedant130701@users.noreply.github.com>
2024-02-08 16:05:51 -0500

    gallery_props.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,13 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import TodoList from './qcomps/todos'
+import Gallery from './qcomps/gallery_props'
 
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <TodoList />
+        <Gallery />
     </div>
   )
 }
--- src/pages/qcomps/gallery_props.js
+++ src/pages/qcomps/gallery_props.js
@@ -1,32 +1,44 @@
-export default function Gallery() {
+function Profile({person, size}) {
   return (
-    <div>
-      <h1>Notable Scientists</h1>
-      <section className="profile">
+    <section className="profile">
         <h2>Maria Skłodowska-Curie</h2>
         <img
-          className="avatar"
-          src='https://i.imgur.com/szV5sdGs.jpg'
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
+        className="avatar"
+        src={'https://i.imgur.com/' + person.imageId + 's.jpg'}
+        alt={person.name}
+        width={size}
+        height={size}
         />
         <ul>
           <li>
             <b>Profession: </b>
-            physicist and chemist
+            {person.profession}
           </li>
           <li>
-            <b>Awards: 4 </b>
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
+            <b>Awards:{person.awardCount} </b>
+            ({person.awards})
           </li>
           <li>
             <b>Discovered: </b>
-            polonium (element)
+            {person.discovery}
           </li>
         </ul>
       </section>
-      <section className="profile">
+  );
+}
+export default function Gallery() {
+  return (
+    <div>
+      <h1>Notable Scientists</h1>
+      <Profile
+      person={{name: "Maria Skłodowska-Curie", imageId: "szV5sdG", profession: "physicist and chemist", awardCount: 4, 
+      awards: "Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal", discovery: "polonium (element)"}}
+      size={70}/>
+      <Profile
+      person={{name: "Katsuko Saruhashi", imageId: "YfeOqp2", profession: "geochemist", awardCount: 2, 
+      awards: "Miyake Prize for geochemistry, Tanaka Prize", discovery: "a method for measuring carbon dioxide in seawater"}}
+      size={70}/>
+      {/* <section className="profile">
         <h2>Katsuko Saruhashi</h2>
         <img
           className="avatar"
@@ -49,7 +61,8 @@ export default function Gallery() {
             a method for measuring carbon dioxide in seawater
           </li>
         </ul>
-      </section>
+      </section> */}
     </div>
   );
+  // So the commented out part was the old one. Same was for marie curie. Redefined to Profile
 }
