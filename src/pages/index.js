67fb6449ef2c137680a342cce3c1ba6589bc7f0e
Vedant Rishi Das <55937481+vedant130701@users.noreply.github.com>
2024-02-16 15:59:01 -0500

    recipes.js in qcomp for you to do

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import List from './qcomps/list_keys_id'
+import List from './qcomps/recipes'
 
 export default function Home() {
   return (
--- src/pages/qcomps/recipes.js
+++ src/pages/qcomps/recipes.js
@@ -13,10 +13,18 @@ export const recipes = [{
 }];
 
 export default function RecipeList() {
+  const recipeList = recipes.map(recipe => 
+    <div key={recipe.id}>
+      <h2>{recipe.name}</h2>
+      <ul>
+        {Array.from(recipe.ingredients).map((ingredient) => 
+        <li key={ingredient}>{ingredient}</li>)}
+      </ul>
+    </div>)
   return (
     <div>
       <h1>Recipes</h1>
-      {}
+      {recipeList}
     </div>
   );
 }
